dd0a92141cf41d89dc33286dcd1099fd9069c69c
Tmwonder1 <fabiandname@gmail.com>
2024-04-19 22:17:59 +0100

    po

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a name-printing function."""
 
-
 def say_my_name(first_name, last_name=""):
     """Print a name.
 
@@ -15,4 +14,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
