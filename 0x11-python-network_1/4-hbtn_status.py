1053028896d1f5ae96b3c6460231170f4e093d78
Tmwonder1 <fabiandname@gmail.com>
2024-07-05 23:19:04 +0100

    cdd

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status
+usage: ./4-hbtn_status.py
 """
-This module fetches the status from a given URL using the 'requests' package.
-It displays the type and content of the response body as specified.
-"""
-
 import requests
 
-def fetch_status(url):
-    """
-    Fetches the status from the provided URL and prints the type and content of the response body.
-    """
-    response = requests.get(url)
-    print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    fetch_status("https://alx-intranet.hbtn.io/status")
+    response = requests.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
