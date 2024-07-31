31cb4e65e368568bc19f70d2ba13b6c708e70323
Tmwonder1 <fabiandname@gmail.com>
2024-07-31 18:16:58 +0100

    vear

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
-const fs = require('fs');
+// Read from file
 
-// Get the file path from command line arguments
-const filePath = process.argv[2];
-
-// Function to read and print the file content
-function readFileAndPrint(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
     if (err) {
-      // Print the error in the desired format
-      console.error(`{ Error: ${err.code}: no such file or directory, open '${err.path}'\n    at Error (native)\n  errno: ${err.errno},\n  code: '${err.code}',\n  syscall: '${err.syscall}',\n  path: '${err.path}' }`);
-    } else {
-      // Print the data if no error
-      console.log(data);
+      console.log(err);
+      return;
     }
+    console.log(data);
   });
-}
-
-// Call the function with the file path
-readFileAndPrint(filePath);
+  
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-// Extract command line arguments for file path and string to write
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-// Function to write the string to the specified file
-function writeStringToFile(path, content) {
-  fs.writeFile(path, content, 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
     if (err) {
-      // If there's an error, print the error object
-      console.error(err);
+      console.log(err);
     }
   });
-}
-
-// Call the function with the provided arguments
-writeStringToFile(filePath, contentToWrite);
+  
